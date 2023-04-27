c9ed62472be965aa94560f51e8e75facd029fb5c
ayamagdy00000 <131966063+ayamagdy00000@users.noreply.github.com>
2023-04-27 16:52:06 +0200

    Add files via upload

---
+++ project.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container =document.querySelector('.container')
+function phones(src){
+    img.src=src;
+}
+function colors(color){
+container.style.background=color;
+}
+
